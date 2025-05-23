c6508df93a95ffa0a44e717bf64be60ec4430478
Parallaxes <=>
2025-05-23 12:37:27 -0700

    Start Challenge 06

--- challenges/src/main.rs
+++ challenges/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     set01::challenge03::run().unwrap();
     set01::challenge04::run().unwrap();
     set01::challenge05::run().unwrap();
+    set01::challenge06::run().unwrap();
 }
---
+++ challenges/src/set01/challenge06.rs
@@ -0,0 +1,4 @@
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    
+    Ok(())
+}
